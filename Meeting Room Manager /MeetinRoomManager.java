f9662913250b37c7cd786e5d230ca029de033d63
SAMY-TY <samystark28@gmail.com>
2025-03-03 15:12:11 +0530

    Create MeetinRoomManager.java

---
+++ Meeting Room Manager /MeetinRoomManager.java
@@ -0,0 +1,64 @@
+package src;
+import java.util.*;
+
+enum RoomFeature {
+    PROJECTOR,
+    VIDEO_CONFERENCING,
+    WHITEBOARD,
+    CONFERENCE_PHONE,
+    AIR_CONDITIONING
+}
+
+class MeetingRoom {
+    String roomId;
+    String roomName;
+    int capacity;
+    EnumSet<RoomFeature> features;
+
+    public MeetingRoom(String roomId, String roomName, int capacity, EnumSet<RoomFeature> features) {
+        this.roomId = roomId;
+        this.roomName = roomName;
+        this.capacity = capacity;
+        this.features = features;
+    }
+}
+
+class RoomScheduler {
+    private final Map<String, MeetingRoom> rooms = new HashMap<>();
+
+    public void addMeetingRoom(MeetingRoom room) {
+        rooms.put(room.roomId, room);
+        System.out.println("Room added: " + room.roomName + ", ID: " + room.roomId);
+    }
+
+    public void bookRoom(String roomId, EnumSet<RoomFeature> requiredFeatures) {
+        MeetingRoom room = rooms.get(roomId);
+        if (room != null && room.features.containsAll(requiredFeatures)) {
+            System.out.println("Room " + room.roomId + " booked successfully.");
+        } else {
+            System.out.println("Room " + roomId + " does not meet the required features.");
+        }
+    }
+
+    public void listAvailableRooms(EnumSet<RoomFeature> requiredFeatures) {
+        List<String> availableRooms = new ArrayList<>();
+        for (MeetingRoom room : rooms.values()) {
+            if (room.features.containsAll(requiredFeatures)) {
+                availableRooms.add(room.roomName);
+            }
+        }
+        System.out.println("Available rooms with " + requiredFeatures + ": " + availableRooms);
+    }
+}
+
+public class MeetingRoomBookingSystem {
+    public static void main(String[] args) {
+        RoomScheduler scheduler = new RoomScheduler();
+        
+        scheduler.addMeetingRoom(new MeetingRoom("001", "Boardroom", 20, EnumSet.of(RoomFeature.PROJECTOR, RoomFeature.CONFERENCE_PHONE, RoomFeature.AIR_CONDITIONING)));
+        scheduler.addMeetingRoom(new MeetingRoom("002", "Strategy Room", 10, EnumSet.of(RoomFeature.WHITEBOARD, RoomFeature.AIR_CONDITIONING)));
+
+        scheduler.bookRoom("001", EnumSet.of(RoomFeature.PROJECTOR, RoomFeature.CONFERENCE_PHONE));
+        scheduler.listAvailableRooms(EnumSet.of(RoomFeature.AIR_CONDITIONING));
+    }
+}
